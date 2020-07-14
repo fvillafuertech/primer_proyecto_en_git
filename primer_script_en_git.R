dcb1dab7ad2322f42066c0825a123c6bbe470409
fvillafuertech <fvillafuerte@gmail.com>
2020-07-13 20:37:17 -0500

    Gráfico de dispersion 2

--- primer_script_en_git.R
+++ primer_script_en_git.R
@@ -8,3 +8,7 @@ iris[,-1]
 # Segunda sección ---------------------------------------------------------
 
 mtcars$mpg
+
+# Tercera sección ---------------------------------------------------------
+
+plot(iris$Sepal.Length, iris$Sepal.Width)
